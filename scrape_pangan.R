22c7a408c5a3e4f934cf4c18d58cc2877010a832
Rekaagustia <rekaagustiaa@gmail.com>
2023-06-14 12:55:31 +0700

    script

---
+++ scrape_pangan.R
@@ -0,0 +1,20 @@
+library(rvest)
+library(tidyverse)
+library(mongolite)
+
+url<-'https://hargapangan.id/tabel-harga/pedagang-besar/daerah'
+beras<-read_html(url)
+beras1 <- beras %>% html_nodes("table") %>% html_table()
+beras1 <- as.data.frame(beras1[[1]])
+
+
+#MONGODB
+message('Input Data to MongoDB Atlas')
+atlas_conn <- mongo(
+  collection = Sys.getenv("ATLAS_COLLECTION"),
+  db         = Sys.getenv("ATLAS_DB"),
+  url        = Sys.getenv("ATLAS_URL")
+)
+
+atlas_conn$insert(beras1)
+rm(atlas_conn)
